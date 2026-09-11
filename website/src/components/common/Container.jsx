ef64c2feed301a725b57311987c1361016e390d8
Samarth Chavda <samarthchavda@example.com>
2026-09-11 17:25:11 +0530

    remove website and merge with the frontend

--- website/src/components/common/Container.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Container = ({ children, className = '', ...props }) => {
-  return (
-    <div className={`website-container ${className}`.trim()} {...props}>
-      {children}
-    </div>
-  );
-};
